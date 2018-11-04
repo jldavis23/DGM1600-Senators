ecd1c84dc1b0d40c4f9e510a6acebc3892f196d7
Jacey Davis <jaceyd923@gmail.com>
2018-11-03 23:21:00 -0600

    Added buttons, header, and commented out lines of JS

--- main.js
+++ main.js
@@ -2,25 +2,24 @@ import { senatorData } from './assets/senators.js'
 
 const senators = senatorData.results[0].members;
 
-const republicans = senators.filter(senator => senator.party === "R")
-const democrats = senators.filter(senator => senator.party === "D")
+// const republicans = senators.filter(senator => senator.party === "R")
+// const democrats = senators.filter(senator => senator.party === "D")
 
-const females = senators.filter(senator => senator.gender === "F")
-const males = senators.filter(senator => senator.gender === "M")
+// const females = senators.filter(senator => senator.gender === "F")
+// const males = senators.filter(senator => senator.gender === "M")
 
-const loyalRepublican = republicans.reduce((acc, senator) => senator.votes_with_party_pct > 0 ? senator : acc, 0)
-const loyalDemocrat = democrats.reduce((acc, senator) => senator.votes_with_party_pct > 0 ? senator : acc, 0)
-
-console.log(`There are ${males.length} men and ${females.length} women in the senate`)
+// const loyalRepublican = republicans.reduce((acc, senator) => senator.votes_with_party_pct > 0 ? senator : acc, 0)
+// const loyalDemocrat = democrats.reduce((acc, senator) => senator.votes_with_party_pct > 0 ? senator : acc, 0)
 
 const senWithPics = senators.map(senator => {
     senator.imgURL = `https://www.govtrack.us/data/photos/${senator.govtrack_id}-100px.jpeg`
     return senator
 })
 
-let pictureDiv = document.querySelector('.container');
+let pictureDiv = document.querySelector('#senator-container');
 
 senWithPics.forEach(senator => {
+    //Insert image and figcaption into the figure element
     let senatorPic = document.createElement('img')
     let senatorFig = document.createElement('figure')
     let senatorCap = document.createElement('figcaption')
